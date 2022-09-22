0e817a70635fe80258de3c9fdf0cf7577b21163e
rvanegas2022 <104838269+rvanegas2022@users.noreply.github.com>
2022-09-22 07:23:51 -0500

    estructura original del proyecto
    estructura original del proyecto

---
+++ proyecto-paquetes-envio/src/main/java/com/example/proyectopaquetesenvio/ProyectoPaquetesEnvioApplication.java
@@ -0,0 +1,13 @@
+package com.example.proyectopaquetesenvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaquetesEnvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaquetesEnvioApplication.class, args);
+	}
+
+}
---
+++ proyecto-paquetes-envio/src/test/java/com/example/proyectopaquetesenvio/ProyectoPaquetesEnvioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.proyectopaquetesenvio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaquetesEnvioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
